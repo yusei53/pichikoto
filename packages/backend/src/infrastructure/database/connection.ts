45a7a4e29063d4963369d33e18723c1dfe4a3bdc
kaitobq <kaitobq520@gmail.com>
2025-06-21 19:58:07 +0900

    feat(db): local and prod connection

--- packages/backend/src/infrastructure/database/connection.ts
+++ packages/backend/src/infrastructure/database/connection.ts
@@ -1,5 +1,29 @@
-import { neon } from "@neondatabase/serverless";
+import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import type { Context } from "hono";
+import ws from "ws";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql);
+// ref: https://neon.com/guides/local-development-with-neon
+export const connectToDatabase = (c: Context) => {
+  let connectionString = c.env.DATABASE_URL!;
+
+  // Configuring Neon for local development
+  if (c.env.NODE_ENV === "development") {
+    connectionString = "postgres://postgres:postgres@db.localtest.me:5432/main";
+    neonConfig.fetchEndpoint = (host) => {
+      const [protocol, port] =
+        host === "db.localtest.me" ? ["http", 4444] : ["https", 443];
+      return `${protocol}://${host}:${port}/sql`;
+    };
+    const connectionStringUrl = new URL(connectionString);
+    neonConfig.useSecureWebSocket =
+      connectionStringUrl.hostname !== "db.localtest.me";
+    neonConfig.wsProxy = (host) =>
+      host === "db.localtest.me" ? `${host}:4444/v2` : `${host}/v2`;
+  }
+  neonConfig.webSocketConstructor = ws;
+  const sql = neon(connectionString);
+
+  const db = drizzle(sql);
+  return db;
+};
