2bc4931e9d0c61ddf8b0aedf7623e2dd24be19c2
sora-00 <sora.umiushi@gmail.com>
2025-06-26 15:10:53 +0900

    feat:UnderLinedTextコンポーネントの作成

---
+++ packages/frontend/src/components/shared/text/UnderlinedText.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+
+type Props = {
+  children: React.ReactNode;
+  width: widthStyle;
+};
+type widthStyle = "sm" | "md" | "lg";
+
+export const UnderlinedText: React.FC<Props> = ({ children, width }) => {
+  const widthClasses = {
+    sm: "w-[185px]",
+    md: "w-[300px]",
+    lg: "w-[600px]"
+  };
+
+  return (
+    <div
+      className={cn(
+        "border-b border-current pb-1 text-sm flex justify-between items-baseline",
+        widthClasses[width]
+      )}
+    >
+      {children}
+    </div>
+  );
+};
