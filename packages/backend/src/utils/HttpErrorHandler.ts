a4807dec005f0c24a5271a251d6b2f6af42a61cd
kaitobq <kaitobq520@gmail.com>
2025-09-21 13:49:45 +0900

    feat(err): create error handler

---
+++ packages/backend/src/utils/HttpErrorHandler.ts
@@ -0,0 +1,38 @@
+import {
+  HttpError,
+  InternalServerError
+} from "@pichikoto/http-contracts/utils";
+import type { Context } from "hono";
+
+type ControllerHandler = (ctx: Context) => Promise<Response>;
+
+const createJsonResponse = (status: number, message: string): Response => {
+  return new Response(JSON.stringify({ error: message }), {
+    status,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+};
+
+export const withHttpErrorHandling = async (
+  c: Context,
+  handler: ControllerHandler
+): Promise<Response> => {
+  try {
+    return await handler(c);
+  } catch (error) {
+    if (error instanceof HttpError) {
+      if (c.env.NODE_ENV === "development") {
+        console.error(`${error.name}: ${error.message}, ${error.cause}`);
+      }
+      return createJsonResponse(error.status, error.message);
+    }
+
+    const err = new InternalServerError(error);
+    if (c.env.NODE_ENV === "development") {
+      console.error(`${err.name}: ${err.message}, ${err.cause}`);
+    }
+    return createJsonResponse(err.status, err.message);
+  }
+};
