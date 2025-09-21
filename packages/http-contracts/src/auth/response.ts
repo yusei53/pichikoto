d94bda42f24f18b5c8dc36e23028c71aaf745fb5
yusei53 <yusei530922@gmail.com>
2025-09-21 23:20:55 +0900

    refactor: response.ts

--- packages/http-contracts/src/auth/response.ts
+++ packages/http-contracts/src/auth/response.ts
@@ -1,45 +1,13 @@
-import { z } from "zod";
+export type VerifyTokenResponse = {
+  
+}
 
-export const callbackResponseSchema = z.object({
-  accessToken: z.string(),
-  refreshToken: z.string()
-});
+export type CallbackResponse = {
+  accessToken: string;
+  refreshToken: string;
+}
 
-export type CallbackResponse = z.infer<typeof callbackResponseSchema>;
-
-export const toCallbackResponse = (
-  accessToken: string,
-  refreshToken: string
-): CallbackResponse => {
-  return callbackResponseSchema.parse({
-    accessToken,
-    refreshToken
-  });
-};
-
-export const refreshTokenResponseSchema = z.object({
-  accessToken: z.string(),
-  refreshToken: z.string()
-});
-
-export type RefreshTokenResponse = z.infer<typeof refreshTokenResponseSchema>;
-
-export const toRefreshTokenResponse = (
-  accessToken: string,
-  refreshToken: string
-): RefreshTokenResponse => {
-  return refreshTokenResponseSchema.parse({
-    accessToken,
-    refreshToken
-  });
-};
-
-export const verifyTokenResponseSchema = z.object({
-  message: z.string()
-});
-
-export type VerifyTokenResponse = z.infer<typeof verifyTokenResponseSchema>;
-
-export const toVerifyTokenResponse = (message: string): VerifyTokenResponse => {
-  return verifyTokenResponseSchema.parse({ message });
-};
+export type RefreshTokenResponse = {
+  accessToken: string;
+  refreshToken: string;
+}
