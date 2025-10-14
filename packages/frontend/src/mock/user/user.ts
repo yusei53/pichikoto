b2c39cf8842b35e98b58adc5dc79303e7cb39eef
naruto1031 <naruto0614@au.com>
2025-10-14 13:23:25 +0900

    fix: remove discordID from mockUser

--- packages/frontend/src/mock/user/user.ts
+++ packages/frontend/src/mock/user/user.ts
@@ -2,7 +2,6 @@ import type { User } from "~/model/user";
 
 export const mockUser: User = {
 	discordUserID: "1",
-	discordID: "test-user-name",
 	discordUserName: "test",
 	discordAvatar: "https://github.com/shadcn.png",
 };
