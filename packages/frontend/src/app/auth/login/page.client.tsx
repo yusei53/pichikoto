bf7561415a424713a838e10e1b4bc521bf5b02e4
yusei53 <yusei530922@gmail.com>
2025-10-18 11:12:54 +0900

    setting favicon

--- packages/frontend/src/app/auth/login/page.client.tsx
+++ packages/frontend/src/app/auth/login/page.client.tsx
@@ -1,21 +1,14 @@
 "use client";
 import React from "react";
-import { Box } from "styled-system/jsx";
+import { Stack } from "styled-system/jsx";
 import { LoginCard } from "~/features/routes/auth/login/LoginCard";
 import { useLogin } from "~/features/routes/auth/login/useLogin";
 
 export const ClientLoginPage: React.FC = () => {
 	const { onLogin } = useLogin();
 	return (
-		<Box
-			minH="100vh"
-			bg="#FAFBFC"
-			display="flex"
-			alignItems="center"
-			justifyContent="center"
-			px="6"
-		>
+		<Stack justifyContent={"center"} alignItems={"center"} height={"100vh"}>
 			<LoginCard onLogin={onLogin} />
-		</Box>
+		</Stack>
 	);
 };
